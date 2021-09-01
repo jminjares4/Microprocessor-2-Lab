bc51f1afbacdb39a25f9dbce174e6156ae890fb7
Your Name <you@example.com>
2021-09-01 10:37:46 -0600

    Updated Lab 1

--- lab_1/main/main.c
+++ lab_1/main/main.c
@@ -4,24 +4,25 @@
 #include "freertos/task.h"
 #include "sdkconfig.h"
 
-// static void vTaskJesus(void *pvParameters){
-//     int *number = (pvParameters);
-//     while(1){
-//         printf("Hello from thread: %d\n", *number);
-//         vTaskDelay(1000 / portTICK_RATE_MS);
-//        // free(number);
-//     }
-// }
+static void vTaskJesus(void *pvParameters){
+    int *number = (pvParameters);
+    int num = *number;
+    while(1){
+        printf("Hello from thread: %d\n", num);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+       // free(number);
+    }
+}
 
-// void app_main(void){
-//     const int *num1 = (int*)1;
-//     const int *num2 = (int*)2;
-//     const int *num3 = (int*)3;
-//     xTaskCreate(vTaskJesus, "Task 1", 512, (void * const)&num1, 2, NULL);
-//     xTaskCreate(vTaskJesus, "Task 2", 512, (void * const)&num2, 3, NULL);
-//     xTaskCreate(vTaskJesus, "Task 3", 512, (void * const)&num3, 4, NULL);
-// }
-
+void app_main(void){
+    const int *num1 = (int*)1;
+    const int *num2 = (int*)2;
+    const int *num3 = (int*)3;
+    xTaskCreate(vTaskJesus, "Task 1", 2048, (void * const)&num1, 2, NULL);
+    xTaskCreate(vTaskJesus, "Task 2", 2048, (void * const)&num2, 3, NULL);
+    xTaskCreate(vTaskJesus, "Task 3", 2048, (void * const)&num3, 4, NULL);
+}
+#if 0
 static void vTask_1(void *pvParameters){
     while(1){
         printf("Task 1\n");
@@ -45,3 +46,4 @@ void app_main(){
     xTaskCreate(vTask_2, "Task 2", 1024, NULL, 4, NULL);    
     xTaskCreate(vTask_3, "Task 3", 1024, NULL, 4, NULL);    
 }
+#endif
