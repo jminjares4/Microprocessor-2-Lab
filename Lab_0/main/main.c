ede8ea5b553bc19e03529f1b9405ee7f6cbee3e6
Your Name <you@example.com>
2021-09-02 11:06:54 -0600

    Added comments to Lab 0

--- Lab_0/main/main.c
+++ Lab_0/main/main.c
@@ -1,3 +1,8 @@
+/*
+    Author: Jesus Minjares
+            Master of Science in Computer Engineering
+    Date:   09-02-2021
+*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -6,15 +11,18 @@
 #define BLINK_GPIO 5
 void app_main(void)
 {
+    /*REset the GPIO pin*/
     gpio_reset_pin(BLINK_GPIO);
-
+    /*Set the direction of the GPIO as OUTPUT*/
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    // gpio_set_pull_mode(BLINK_GPIO, GPIO_PULLDOWN_ONLY);
+
     while(1){
+        /*Turn on the LED*/
         printf("Turning  on LED...\n");
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_RATE_MS);
 
+        /*Turn off the LED*/
         printf("Turning off LED...\n");
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_RATE_MS);
