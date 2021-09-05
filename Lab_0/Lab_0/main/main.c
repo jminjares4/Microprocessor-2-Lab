204fe76eb3dfd52c53fa8cfebf1b942bbea1e869
Your Name <you@example.com>
2021-09-05 10:40:01 -0600

    Added pinout

--- Lab_0/Lab_0/main/main.c
+++ Lab_0/Lab_0/main/main.c
@@ -8,6 +8,33 @@
                 of the bonus. The code will print to the screen every second 
                 "Hello world!" and turn off and on the LED for 2 and 3 second respectively.
     Date:       09-05-2021
+
+    Pinout:
+
+                                         +-----------------------+
+                                         | O      | USB |      O |
+                                         |        -------        |
+                                     3V3 | [ ]               [ ] | VIN
+                                     GND | [ ]               [ ] | GND
+     Touch3 / HSPI_CS0 / ADC2_3 / GPIO15 | [ ]               [ ] | GPIO13 / ADC2_4 / HSPI_ID / Touch4
+ CS / Touch2 / HSPI_WP / ADC2_2 /  GPIO2 | [ ]               [ ] | GPIO12 / ADC2_5 / HSPI_Q / Touch5
+      Touch0 / HSPI_HD / ADC2_0 /  GPIO4 | [ ]               [ ] | GPIO14 / ADC2_6 / HSPI_CLK / Touch6
+                         U2_RXD / GPIO16 | [ ]               [ ] | GPIO27 / ADC2_7 / Touch7
+                         U2_TXD / GPIO17 | [ ]               [ ] | GPIO26 / ADC2_9 / DAC2
+                      V_SPI_CS0 /  GPIO5 | [ ]  ___________  [ ] | GPIO25 / ADC2_8 / DAC1
+                SCK / V_SPI_CLK / GPIO18 | [ ] |           | [ ] | GPIO33 / ADC1_5 / Touch8 / XTAL32
+        U0_CTS / MSIO / V_SPI_Q / GPIO19 | [ ] |           | [ ] | GPIO32 / ADC1_4 / Touch9 / XTAL32
+                 SDA / V_SPI_HD / GPIO21 | [ ] |           | [ ] | GPIO35 / ADC1_7 
+                  CLK2 / U0_RXD /  GPIO3 | [ ] |           | [ ] | GPIO34 / ADC1_6 
+                  CLK3 / U0_TXD /  GPIO1 | [ ] |           | [ ] | GPIO39 / ADC1_3 / SensVN 
+        SCL / U0_RTS / V_SPI_WP / GPIO22 | [ ] |           | [ ] | GPIO36 / ADC1_0 / SensVP 
+                MOSI / V_SPI_WP / GPIO23 | [ ] |___________| [ ] | EN 
+                                         |                       |
+                                         |  |  |  ____  ____  |  |
+                                         |  |  |  |  |  |  |  |  |
+                                         |  |__|__|  |__|  |__|  |
+                                         | O                   O |
+                                         +-----------------------+
 */
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
