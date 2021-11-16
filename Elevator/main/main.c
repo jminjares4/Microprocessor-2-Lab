a8ca969a98a472823d442fd90478df5a2109b039
jminjares4 <jminjares4@miners.utep.edu>
2021-11-15 21:44:46 -0700

    Added external UART

--- Elevator/main/main.c
+++ Elevator/main/main.c
@@ -14,58 +14,61 @@
 #include "sdkconfig.h"
 #include <string.h>
 #include "esp_log.h"
-#define UART_0_TX 1
-#define UART_0_RX 3
 
+// ESP32
+
+// --Sensors--
+// Accerlometer
+// motion sensor
+// ?
+
+// --Hardware--
+// SG90 Servo
+// LEDs
+// Buttons
+// Resistors
+// External Header Pins / UART  DONE!!!!
+
+#define UART_2_TX 17
+#define UART_2_RX 16
 #define UART_BUFFER_SIZE 1024
-const uart_port_t uart_num = UART_NUM_0;
+const uart_port_t uart_num = UART_NUM_2;
 void uart_setup(void)
 {
-
     /* Configure parameters of an UART driver,
      * communication pins and install the driver */
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_APB,
+        uart_config_t uart_config = {
+        .baud_rate = 9600,                     // defualt buadrate
+        .data_bits = UART_DATA_8_BITS,         // 8 bits
+        .parity = UART_PARITY_DISABLE,         // disable parity
+        .stop_bits = UART_STOP_BITS_1,         // 1 stop bit
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE, // disable hardware flow control
+        .source_clk = UART_SCLK_APB,           // set default clock
     };
-
+    // Initialize uart configuration @ uart_num
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
-
-    // Set UART pins(TX: IO4, RX: IO5, RTS: IO18, CTS: IO19)
-    ESP_ERROR_CHECK(uart_set_pin(uart_num, UART_0_TX, UART_0_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-
+    // Set uart pins for UART0
+    ESP_ERROR_CHECK(uart_set_pin(uart_num, UART_2_TX, UART_2_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    // Install uart driver with a BUFFER * 2 size
     ESP_ERROR_CHECK(uart_driver_install(uart_num, UART_BUFFER_SIZE * 2, UART_BUFFER_SIZE * 2, 0, NULL, 0));
 }
-#define TAG "RX TAG"
+
 void uart_task(void *arg)
 {
+    const uart_port_t uart_num = UART_NUM_2;
     while (1)
     {
         // Write data to UART.
-        // char *test_str = "This is a test string.\r\n";
-        // uart_write_bytes(uart_num, (const char *)test_str, strlen(test_str));
-        // vTaskDelay(1000 / portTICK_RATE_MS);
-        // Read data from UART.
-        const uart_port_t uart_num = UART_NUM_0;
-        uint8_t data[1];
-        int length = 0;
-        ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t *)&length));
-        length = uart_read_bytes(uart_num, data, length, 100);
-        data[length] =0;
-        uart_write_bytes(uart_num, (const char *)data, 1);
-        
-        // ESP_LOGI(TAG, "bytes: '%s'", data);
-        // ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
-        //  vTaskDelay(100 / portTICK_RATE_MS);
+        char *test_str = "ADC Data\r\nDoor Data\r\nSensor Data\r\nCurrent Floor\r\nStatus\r\n";
+        uart_write_bytes(uart_num, (const char *)test_str, strlen(test_str));
+        vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
 
 void app_main(void)
 {
+    // Initialize UART 2 for external serial communication
     uart_setup();
+    // Initialize UART task which would be Task 5 for debugging all status of sensors, doors, and current stage
     xTaskCreate(uart_task, "uart_task", 2048, NULL, 10, NULL);
 }
